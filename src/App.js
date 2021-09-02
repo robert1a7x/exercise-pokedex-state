447ce8b7730d43afc6ca4609a50057563c4c8475
Robert Nascimento <robert1a7x@gmail.com>
2021-09-02 20:30:40 -0300

    Exercicio 1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.changePokemon = this.changePokemon.bind(this);
+
+    this.state = {
+      currentPokemon: 0
+    }
+  }
+
+  changePokemon() {
+    if (this.state.currentPokemon === pokemons.length - 1) {
+      this.setState({ currentPokemon: 0 });
+    } else {
+      this.setState({ currentPokemon: this.state.currentPokemon + 1 })
+    }
+  }
+
+  render() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <Pokedex pokemons={pokemons[this.state.currentPokemon]} />
+      <button onClick={this.changePokemon}>Próximo Pokemon</button>
     </div>
   );
+  }
 }
 
 export default App;
