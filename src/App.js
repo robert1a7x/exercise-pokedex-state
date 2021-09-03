a3417ebf2d55fcf17e4a60834fd1c67f102e7d15
Robert Nascimento <robert1a7x@gmail.com>
2021-09-03 00:10:18 -0300

    Exercicio 2 e alguns bunus + css

--- src/App.js
+++ src/App.js
@@ -8,26 +8,48 @@ class App extends React.Component {
     super();
 
     this.changePokemon = this.changePokemon.bind(this);
+    this.filterPokemon = this.filterPokemon.bind(this);
+    this.allPokemons = this.allPokemons.bind(this);
 
     this.state = {
+      pokemonArray: [...pokemons],
       currentPokemon: 0
     }
   }
 
   changePokemon() {
-    if (this.state.currentPokemon === pokemons.length - 1) {
+    this.setState((prevState, _props) => ({
+      currentPokemon: prevState.currentPokemon + 1
+    }))
+    if (this.state.currentPokemon === this.state.pokemonArray.length - 1) {
       this.setState({ currentPokemon: 0 });
-    } else {
-      this.setState({ currentPokemon: this.state.currentPokemon + 1 })
     }
+  }
+
+  filterPokemon(type) {
+    this.setState({currentPokemon: 0});
+    this.setState({pokemonArray: pokemons.filter((pokemon) => pokemon.type === type)});
+  }
+
+  allPokemons() {
+    this.setState({currentPokemon: 0});
+    this.setState({pokemonArray: pokemons});
   }
 
   render() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons[this.state.currentPokemon]} />
+      <Pokedex pokemons={this.state.pokemonArray[this.state.currentPokemon]} />
+      <button onClick={this.allPokemons}>Todos</button>
       <button onClick={this.changePokemon}>Próximo Pokemon</button>
+      <button onClick={() => {this.filterPokemon('Fire')}}>Fogo</button>
+      <button onClick={() => {this.filterPokemon('Psychic')}}>Psíquico</button>
+      <button onClick={() => {this.filterPokemon('Electric')}}>Elétrico</button>
+      <button onClick={() => {this.filterPokemon('Bug')}}>Inseto</button>
+      <button onClick={() => {this.filterPokemon('Poison')}}>Veneno</button>
+      <button onClick={() => {this.filterPokemon('Normal')}}>Normal</button>
+      <button onClick={() => {this.filterPokemon('Dragon')}}>Dragão</button>
     </div>
   );
   }
